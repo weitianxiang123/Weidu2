681e9c6ede2d1bae32d75f46e155dfbdffbc3895
weitianxiang123 <1815835822@qq.com>
2018-11-27 18:15:14 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bw.movie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //我是维度影院项目组
+
+    }
+}
